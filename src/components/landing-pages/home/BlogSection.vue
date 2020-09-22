23de332544e495c056a574a7ab84a436cf4697b8
Tortu <tortuc611@gmail.co>
2020-09-23 00:58:57 +0800

    BlogSection.vue finish

--- src/components/landing-pages/home/BlogSection.vue
+++ src/components/landing-pages/home/BlogSection.vue
@@ -3,78 +3,30 @@
     <section class="blog-area ptb-80">
         <div class="container">
             <div class="section-title">
-                <h2>The News from Our Blog</h2>
+                <h2>{{$t('BlogTitle')}}</h2>
                 <div class="bar"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>{{$t('BlogSubTitle')}}</p>
             </div>
 
             <div class="row">
-                <div class="col-lg-4 col-md-6">
+                <div v-for="item in myItems" :key="item.id" class="col-lg-4 col-md-6">
                     <div class="single-blog-post">
                         <div class="blog-image">
                             <a href="#">
-                                <img src="../../../assets/img/blog-image/1.jpg" alt="image">
+                                <img :src="imgUrl(item.img)" alt="image">
                             </a>
 
                             <div class="date">
-                                <i data-feather="calendar"></i> March 15, 2019
+                                <i data-feather="calendar"></i> {{item.date}}
                             </div>
                         </div>
 
                         <div class="blog-post-content">
-                            <h3><a href="#">The security risks of changing package owners</a></h3>
+                            <h3><a href="#">{{item.title}}</a></h3>
 
                             <span>by <a href="#">admin</a></span>
 
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
-
-                            <a href="#" class="read-more-btn">Read More <feather type="arrow-right"></feather> </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="blog-image">
-                            <a href="#">
-                                <img src="../../../assets/img/blog-image/2.jpg" alt="image">
-                            </a>
-
-                            <div class="date">
-                                <i data-feather="calendar"></i> March 17, 2019
-                            </div>
-                        </div>
-
-                        <div class="blog-post-content">
-                            <h3><a href="#">Tips to Protecting Your Business and Family</a></h3>
-
-                            <span>by <a href="#">smith</a></span>
-
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
-
-                            <a href="#" class="read-more-btn">Read More <feather type="arrow-right"></feather> </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
-                    <div class="single-blog-post">
-                        <div class="blog-image">
-                            <a href="#">
-                                <img src="../../../assets/img/blog-image/3.jpg" alt="image">
-                            </a>
-
-                            <div class="date">
-                                <i data-feather="calendar"></i> March 19, 2019
-                            </div>
-                        </div>
-
-                        <div class="blog-post-content">
-                            <h3><a href="#">Protect Your Workplace from Cyber Attacks</a></h3>
-
-                            <span>by <a href="#">john</a></span>
-
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                            <p>{{item.content}}</p>
 
                             <a href="#" class="read-more-btn">Read More <feather type="arrow-right"></feather> </a>
                         </div>
@@ -88,6 +40,38 @@
 
 <script>
     export default {
-        name: 'HowItWorks'
+        name: 'HowItWorks',
+        data(){
+            return {
+                myItems: [
+                    {
+                        id: 1,
+                        img: '1.jpg',
+                        title: 'Jason',
+                        date: '2020.1.23',
+                        content: 'Jason started from 2020.1 in upwork.com and he earned one billion in one month. so How his do it?'
+                    },
+                    {
+                        id: 2,
+                        img: '1.jpg',
+                        title: 'Jason',
+                        date: '2020.1.23',
+                        content: 'Jason started from 2020.1 in upwork.com and he earned one billion in one month. so How his do it?'
+                    },
+                    {
+                        id: 3,
+                        img: '1.jpg',
+                        title: 'Jason',
+                        date: '2020.1.23',
+                        content: 'Jason started from 2020.1 in upwork.com and he earned one billion in one month. so How his do it?'
+                    }
+                ]
+            }
+        },
+        methods: {
+            imgUrl(url){
+                return require('../../../assets/img/blog-section/' + url)
+            }
+        }
     }
 </script>
