bc91af14a7ec1d32437d475b4c77ecf5ed3cca12
Tortu <tortuc611@gmail.co>
2020-09-22 22:44:42 +0800

    Service finish

--- src/components/landing-pages/home/Services.vue
+++ src/components/landing-pages/home/Services.vue
@@ -1,74 +1,66 @@
 <template>
-    <!-- Start Repair Services Area -->
-    <section class="repair-services-area ptb-80 bg-f7fafd">
+    <!-- Start IoT Why Choose Us Area -->
+    <section class="iot-why-choose-us ptb-80 pb-0">
         <div class="container">
             <div class="section-title">
-                <h2>Our Featured Services that We Provide</h2>
+                <h2>{{$t('WhyUsRowOne')}}</h2>
                 <div class="bar"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>{{$t('WhyUsRowTwo')}}</p>
             </div>
-            
+
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-repair-services bg1">
+                    <div class="single-iot-box">
                         <div class="icon">
-                            <i class="flaticon-monitor"></i>
+                            <img src="../../../assets/img/icon1.png" alt="image">
                         </div>
-
-                        <h3>Laptop Repair</h3>
-                        <p>Lorem ipsum eiusmod dolor sit amet elit, adipiscing, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
-                        
-                        <a href="#"><i data-feather="arrow-right"></i></a>
+                        <h3>{{$t('AutomaticMatchingPlatform')}}</h3>
+                        <p>{{$t('WhyUsColOne')}}</p>
                     </div>
                 </div>
 
                 <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-repair-services bg2">
+                    <div class="single-iot-box">
                         <div class="icon">
-                            <i class="flaticon-idea"></i>
+                            <img src="../../../assets/img/icon2.png" alt="image">
                         </div>
-
-                        <h3>Computer Repair</h3>
-                        <p>Lorem ipsum eiusmod dolor sit amet elit, adipiscing, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
-                        
-                        <a href="#"><i data-feather="arrow-right"></i></a>
+                        <h3>{{$t('SmartBackground')}}</h3>
+                        <p>{{$t('WhyUsColTwo')}}</p>
                     </div>
                 </div>
 
                 <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-repair-services bg3">
+                    <div class="single-iot-box">
                         <div class="icon">
-                            <i class="flaticon-layout"></i>
+                            <img src="../../../assets/img/icon3.png" alt="image">
                         </div>
-
-                        <h3>Apple Products Repair</h3>
-                        <p>Lorem ipsum eiusmod dolor sit amet elit, adipiscing, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
-                        
-                        <a href="#"><i data-feather="arrow-right"></i></a>
+                        <h3>{{$t('SuccessGuarantee')}}</h3>
+                        <p>{{$t('WhyUsColThr')}}</p>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-repair-services bg4">
+                <div class="col-lg-3 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+                    <div class="single-iot-box">
                         <div class="icon">
-                            <i class="flaticon-update-arrows"></i>
+                            <img src="../../../assets/img/icon8.png" alt="image">
                         </div>
-
-                        <h3>Software Update</h3>
-                        <p>Lorem ipsum eiusmod dolor sit amet elit, adipiscing, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
-                        
-                        <a href="#"><i data-feather="arrow-right"></i></a>
+                        <h3>{{$t('SolveLegalIssues')}}</h3>
+                        <p>{{$t('WhyUsColFor')}}</p>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
-    <!-- End Repair Services Area -->
+    <!-- End IoT Why Choose Us Area -->
 </template>
 
 <script>
-export default {
-    name: 'Services'
-}
+    export default {
+        name: 'WhyChooseUs'
+    }
 </script>
+<style scoped>
+    .single-iot-box {
+        min-height: 461px;
+    }
+</style>
